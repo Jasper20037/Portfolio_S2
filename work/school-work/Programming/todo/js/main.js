97fc8858c4a9264664ddb5f59ec4f6f3631a0841
Jasper van den Heuvel <jasperheuvel13@gmail.com>
2023-06-16 10:14:43 +0200

    added project and UCD work

--- work/school-work/Programming/todo/js/main.js
+++ work/school-work/Programming/todo/js/main.js
@@ -5,31 +5,78 @@ const taskContainer = document.getElementById('task-container');
 const inputTask = document.getElementById('input-task');
 // Array for task
 const taskStorage = [];
+let task = "";
+let li = "";
+let checkButton = "";
+let deleteButton = "";
 
 // Forreach to get local storage
 
 // Event listener for add button
-addTask.addEventListener('click', function(){
 
+function createTask(taskStorage) {
     // Creating the task
-    let task = document.createElement('article');
+    task = document.createElement('article');
     task.classList.add('task');
 
-    let li = document.createElement('li');
+    li = document.createElement('li');
     li.innerText = `${inputTask.value}`;
     task.appendChild(li);
 
     // Creating the check button
-    let checkButton = document.createElement("button");
+    checkButton = document.createElement("button");
     checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
     checkButton.classList.add('checkTask');
     task.appendChild(checkButton)
 
     // Creating the delete button
-    let deleteButton = document.createElement("button");
+    deleteButton = document.createElement("button");
     deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
     deleteButton.classList.add('deleteTask');
     task.appendChild(deleteButton);
+}
+
+
+window.addEventListener('load', function() {
+    // Get local storage
+    localTaskStorage = JSON.parse(localStorage.getItem("taskList"));
+
+    // Forreach to get local storage
+    localTaskStorage.forEach(element => {
+        createTask();
+        taskContainer.appendChild(task);
+        li.innerText = `${element}`;
+    });
+
+    checkButton.addEventListener('click', function(){
+
+        // Set a line through the task when the checkbutton is clicked
+        checkButton.parentElement.style.textDecoration = "line-through";
+        checkButton.style.backgroundColor = "orange"
+        checkButton.style.color = "white"
+        checkButton.style.borderColor = "white"
+        checkButton.parentElement.style.transition = ".5s"
+    });
+
+    // Does not work
+    // deleteButton.addEventListener('click', function(e){
+
+    //     // Get the element that is clicked (delete button)
+    //     let target = e.target;
+
+    //     // Traverse to the grandparent of the clicked element
+    //     // Parent one is list item, parent two is the task
+    //     target.parentElement.parentElement.remove();
+
+    //     // Remove from array
+
+    // });
+});
+
+
+addTask.addEventListener('click', function(){
+
+    createTask();
 
     // Store in array
     taskStorage.push(inputTask.value)
